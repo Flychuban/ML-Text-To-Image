8d807cdb94fac5399ebada7f9daeea4af50e1450
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-05-26 21:51:04 +0300

    stable diffusion created

--- app.py
+++ app.py
@@ -20,8 +20,19 @@ prompt.place(x=10, y=10)
 lmain = ctk.CTkLabel(app, height=512, width=512)
 lmain.place(x=10, y=110)
 
+modelid = "runwayml/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, torch_dtype=torch.float16, use_auth_token=auth_token)
+pipe.to('cpu')
 
-trigger = ctk.CTkButton(app, text="Generate", height=40, width=120, font=("Arial", 20), fg_color='blue', text_color='white')
+def generate():
+    with autocast('cpu'):
+        image = pipe(prompt.get(), guidance_scale=8.5).images[0]
+    
+    image.save("generated_img.png")
+    img = ImageTk.PhotoImage(image)
+    lmain.configure(image=img)
+
+trigger = ctk.CTkButton(app, text="Generate", height=40, width=120, font=("Arial", 20), fg_color='blue', text_color='white', command=generate)
 trigger.place(x=206, y=60)
 
 app.mainloop()
